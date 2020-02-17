bafd85b39bf181651b5ff1d744f0af9fc59fb8de
Samuel <samuel@umich.edu>
2020-02-17 14:03:05 -0500

    generate scene programs

--- generate_script.py
+++ generate_script.py
@@ -11,35 +11,41 @@ from utils import CC, pre                               # ansi
 from utils import secs_endured, megs_alloced            # profiling
 from utils import reseed, bernoulli, geometric, uniform # math
 
-from lg_types import tInt, tCell, tColor, tBlock, tGrid, tDir
+from lg_types import tInt, tCell, tColor, tShape, tBlock, tGrid, tDir, tNoise
 from lg_types import tCount_, tFilter_, tArgmax_, tMap_, tRepeat_
 
 #=============================================================================#
 #=====  0. PROVER  ===========================================================#
 #=============================================================================#
 
-signatures_by_nm = {
+sigs_by_nm = {
     # basic samplers:
-    'make_sml': tInt,
-    'make_big': tInt,
-    'make_cel': tCell,
-    'make_col': tColor,
-    'make_dir': tDir,
-    'make_blk': tBlock.frm(tInt),
+    #'one'     : tInt.frm(tNoise),                     # [1]
+    'gen_some': tInt.frm(tNoise),                      # [1,2,3]
+    'gen_svrl': tInt.frm(tNoise),                      # [3,4,5]
+    'gen_many': tInt.frm(tNoise),                      # [15...30] 
+    'gen_cell': tCell.frm(tGrid).frm(tNoise),          # uniform cell
+    'gray'    : tColor,                                # 'A'
+    'gen_rain': tColor.frm(tNoise),                    # GENERIC_COLORS
+    'gen_shap': tShape.frm(tInt).frm(tNoise),          # get random shape 
+    # product types:
+    'blok_cons': tBlock.frm(tShape).frm(tColor),
+    'shap_blok': tShape.frm(tBlock),
+    'colr_blok': tColor.frm(tBlock),
     # render a grid:
-    'blnk_grd': tGrid.frm(tInt).frm(tInt),
-    'rndr_blk': tGrid.frm(tBlock).frm(tCell).frm(tColor).frm(tGrid),
-    'rndr_blks': tGrid.frm(tBlock.s()).frm(tCell.s()).frm(tColor.s()).frm(tGrid),
-    # numerical concepts:
-    'size_blks': tCount_(tBlock),
-    'size_blk': tInt.frm(tBlock),
-    'amax_blks': tArgmax_(tBlock),
-    # list helpers:
-    'sing_cols': tColor.s().frm(tColor),
-    'cons_cols': tColor.s().frm(tColor).frm(tColor.s()),
-    'sing_cels': tCell.s().frm(tCell),
-    'cons_cels': tCell.s().frm(tCell).frm(tCell.s()),
-    'sing_blks': tBlock.s().frm(tBlock),
+    'fill_grd':  tGrid.frm(tCell).frm(tColor),
+    'noise_grd': tGrid.frm(tColor).frm(tNoise).frm(tGrid),      # sparse noise
+    'blnk_grd':  tGrid.frm(tInt).frm(tInt),                     # black with given height, width
+    'rndr_blks': tGrid.frm(tBlock.s()).frm(tGrid).frm(tNoise),
+    'rndr_blk':  tGrid.frm(tBlock).frm(tGrid),                  # put block in upper right corner
+    ## numerical concepts:
+    'size_shap':  tInt.frm(tShape),
+    'amax_blocks': tArgmax_(tBlock),
+    #'amax_shapes': tArgmax_(tShape),
+    ## list helpers:
+    #'sing_cols': tColor.s().frm(tColor),
+    #'cons_cols': tColor.s().frm(tColor).frm(tColor.s()),
+    'gen_blks': tBlock.s().frm(tNoise).frm(tBlock.frm(tNoise)).frm(tInt),
     'cons_blks': tBlock.s().frm(tBlock).frm(tBlock.s()),
 }
 
@@ -50,18 +56,21 @@ def get_fresh():
     return str(var_count)
 
 verbose = False
+lines = 0 
 
 def construct(goal, resources):
+    global lines
     if verbose:
         print('analyzing {}'.format(str(goal)))
+        lines += 1
 
-    if bernoulli(0.001):
+    if bernoulli(1e-4):
         pre(False, 'timeout')
 
-    if bernoulli(0.1):
-        split = uniform([tInt, tCell, tColor, tBlock, tGrid])
-        if bernoulli(0.5):
-            split = split.s()
+    if bernoulli(0.1): # split
+        split = uniform([
+            tInt, tColor, tShape, tShapes, tBlock, tBlocks, tGrid
+        ])
         arg  = construct(split, resources)
 
         var_nm = get_fresh()
@@ -70,11 +79,12 @@ def construct(goal, resources):
 
         body = construct(goal, resources)
         return '(({}->{})({}))'.format(var_nm, body, arg)
-
-    elif goal.kind=='prod':
-        fst = construct(goal.fst, resources)
-        snd = construct(goal.snd, resources)
-        return '({}; {})'.format(fst, snd)
+    elif goal.kind=='from' and bernoulli(0.8): # destruct
+        var_nm = get_fresh()
+        resources = {k:v for k,v in resources.items()}
+        resources[var_nm] = goal.arg
+        body = construct(goal.out, resources)
+        return '({}->{})'.format(var_nm, body)
     else:
         matches = [
             (nm,sig,hypoths) for nm,sig in resources.items()
@@ -85,22 +95,55 @@ def construct(goal, resources):
             nm, sig, hypoths = uniform(matches) 
             if verbose:
                 print('matched {} with {}'.format(goal, nm))
+                lines += 1
             hypoths = [' '+construct(h, resources) for h in hypoths]
-            return '({}{})'.format(
-                nm, ''.join(hypoths)
+            return (
+                nm if not hypoths else
+                '({}{})'.format(nm, ''.join(hypoths))
             )
         else:
             pre(False, 'no match found')
 
-def tenacious_construct(goal):
-    global var_count
-    while True:
+def tenacious_construct(goal, add_resources={}):
+    global var_count, lines
+
+    resources = {k:v for k,v in sigs_by_nm.items()}
+    for k,v in add_resources.items():
+        resources[k] = v
+
+    for i in range(10**3):
+        if verbose:
+            print()
+            print()
         try:
-            code = construct(goal, resources=signatures_by_nm)
-            pre(50 <= len(code) <= 500, 'code too long or short')
+            code = construct(goal, resources)
+            #pre(50 <= len(code) <= 500, 'code too long or short')
             return code
         except:
             var_count=0
+            #print(CC+('@^ '+' '*100)*(lines+3))
+            lines = 0 
             continue
 
-print(tenacious_construct(tGrid.times(tGrid)))
+print('blocks:'  , tenacious_construct(tBlock.s(), {'noise':tNoise,                                                  }))
+print('getblock:', tenacious_construct(tBlock,     {                'blocks':tBlock.s()                              }))
+print('grid:'    , tenacious_construct(tGrid,      {'noise':tNoise, 'blocks':tBlock.s(), 'block':tBlock,             }))
+print('X:'       , tenacious_construct(tGrid,      {'noise':tNoise,                      'block':tBlock, 'grid':tGrid}))
+print('Y:'       , tenacious_construct(tGrid,      {                                     'block':tBlock, 'grid':tGrid}))
+
+# future:
+#'fit_shap': tCell.frm().frm(tGrid).frm(tNoise),    # get fitting location for shape 
+#'rndr_blk': tGrid.frm(tBlock).frm(tCell).frm(tGrid),
+#'gen_crnr': tCell.frm(tGrid)                       # upper left cell
+#'rndr_blk': tGrid.frm(tBlock).frm(tGrid),
+#'size_blks': tCount_(tBlock),
+#'sing_cels': tCell.s().frm(tCell),
+#'cons_cels': tCell.s().frm(tCell).frm(tCell.s()),
+#'gen_card': tDir.frm(tNoise),                      # [ferz directions] 
+#'black'   : tColor,                                # 'K'
+
+#elif goal.kind=='prod':
+#    fst = construct(goal.fst, resources)
+#    snd = construct(goal.snd, resources)
+#    return '({}; {})'.format(fst, snd)
+
