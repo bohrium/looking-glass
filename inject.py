5a193d03048d90ba6c09bfab6817b2486989101e
Samuel <samuel@umich.edu>
2020-02-28 23:57:17 -0500

    all tests passed!  dependency checker done!

--- inject.py
+++ inject.py
@@ -20,7 +20,66 @@ from fit_weights import WeightLearner
 from resources import PrimitivesWrapper
 from vis import str_from_grids, render_color
 
-class DependencyValue:
+class DepType:
+    def __init__(self, kind, **kwargs):
+        pre(kind in ['base', 'from', 'pair'], 'unknown kind `{}`'.format(kind)) 
+        self.kind = kind
+        if self.kind=='base':
+            pass
+        elif self.kind=='from':
+            self.out = kwargs['out'] 
+            self.arg = kwargs['arg'] 
+        elif self.kind == 'pair':
+            self.fst = kwargs['fst'] 
+            self.snd = kwargs['snd'] 
+    def frm(self, rhs):
+        return DepType('from', out=self, arg=rhs)
+    def pair(self, rhs):
+        return DepType('pair', fst=self, snd=rhs)
+
+    def __repr__(self):
+        if self.kind=='base': return 'BASE'
+        elif self.kind == 'pair':
+            return '({},{})'.format(repr(self.fst), repr(self.snd))
+        elif self.kind=='from':
+            return '({} <- {})'.format(repr(self.out), repr(self.arg))
+
+
+
+    def build(self, leaves=set()):
+        '''
+            Return value of this type maximally sensitive to given set of
+            ints
+        '''
+        if self.kind=='base':
+            return DepValue('base',
+                members = leaves
+            )
+        elif self.kind == 'pair':
+            return DepValue('pair',
+                fst = self.fst.build(leaves),
+                snd = self.snd.build(leaves)
+            )
+        elif self.kind=='from':
+            return DepValue('from',
+                call_on = lambda x: self.out.build(leaves.union(x.squash())),
+                default = self.out.build(leaves)
+            )
+
+
+class DepValue:
+
+    def squash(self):
+        '''
+            Return set of ints on which, in isolation, this depvalue depends
+        '''
+        if self.kind=='base':
+            return self.members
+        elif self.kind == 'pair':
+            return self.fst.squash().union(self.snd.squash())
+        elif self.kind=='from':
+            return self.default.squash() 
+
     def __init__(self, kind, **kwargs):
         pre(kind in ['base', 'from', 'pair'], 'unknown kind `{}`'.format(kind)) 
 
@@ -33,9 +92,10 @@ class DependencyValue:
             self.snd = kwargs['snd'] 
         elif self.kind=='from':
             self.call_on = kwargs['call_on']
+            self.default = kwargs['default']
 
     def pair(self, rhs):
-        return DependencyValue('pair',
+        return DepValue('pair',
             fst=self,
             snd=rhs
         )
@@ -46,36 +106,38 @@ class DependencyValue:
         elif self.kind == 'pair':
             return '({},{})'.format(repr(self.fst), repr(self.snd))
         elif self.kind=='from':
-            return 'FUNC'
+            return 'F{}'.format(repr(self.default))
 
     def __eq__(self, rhs):
         return repr(self)==repr(rhs)
 
-    def union(self, rhs):
-        if self.kind=='base':
-            return DependencyValue(self.kind,
-                members=self.members.union(rhs.members)
-            )
-        elif self.kind=='pair':
-            return DependencyValue(self.kind,
-                self.fst.union(rhs.fst),
-                self.snd.union(rhs.snd)
-            )
-        elif self.kind=='from':
-            return DependencyValue(self.kind,
-                call_on = lambda x:
-                    self.call_on(x).union(rhs.call_on(x)),
-            )
+    #def union(self, rhs):
+    #    if self.kind=='base':
+    #        return DepValue(self.kind,
+    #            members=self.members.union(rhs.members)
+    #        )
+    #    elif self.kind=='pair':
+    #        return DepValue(self.kind,
+    #            self.fst.union(rhs.fst),
+    #            self.snd.union(rhs.snd)
+    #        )
+    #    elif self.kind=='from':
+    #        return DepValue(self.kind,
+    #            call_on = lambda x:
+    #                self.call_on(x).union(rhs.call_on(x)),
+    #        )
+
+tBase = DepType('base')
 
 def base(*args):
-    return DependencyValue('base', members=set(args))
+    return DepValue('base', members=set(args))
 
-class DependencyAnalyzer:
-    def __init__(self, sensitives, sigs_by_nm, verbose=False):
+class DepAnalyzer:
+    def __init__(self, sensitives, environ, verbose=False):
         self.sensitives = sensitives
-        self.sigs_by_name = sigs_by_nm
+        self.environ = environ
         self.verbose = verbose
-
+    
     def report(self, tag, dst, src, env, depth):
         ''' Print the small-step substitution specified in arguments. '''
         if not self.verbose: return
@@ -84,36 +146,38 @@ class DependencyAnalyzer:
                 tag, dst, src, env
             )
         ))
-
-    def abstract_eval(self, tree, environ={}, depth=0): 
+    
+    def abstract_eval(self, tree, environ=None, depth=0): 
         '''
             Abstractly interpret `tree` to find subset of leaves in
             self.sensitives that could affect tree's value.
         '''
-        #if type(tree) in (set, types.FunctionType):
-        #    return tree
-        if type(tree) == DependencyValue:
-            return tree
+        if environ is None:
+            environ = self.environ
 
+        if type(tree) == DepValue:
+            return tree
         elif type(tree) == str:
-            if tree in sensitives:
-                return DependencyValue('base', members={tree})
-            elif tree in environ:
-                impl = environ[tree][0]
-                return self.abstract_eval(impl, environ, depth+1)
-            else:
-                return DependencyValue('base', members=set())
+            if tree in sensitives: return base(tree)
+            elif tree in environ: return self.abstract_eval(environ[tree], environ, depth+1)
+            else: return base()
         elif type(tree) == dict:
             for (var_nm, var_type), body in tree.items():
-                return DependencyValue('from', call_on=(lambda x:
-                    self.abstract_eval(body, {
-                        k:v for k,v in
-                        list(environ.items())+[(var_nm, (x, var_type))]
-                    }, depth+1)
-                ))
+                return DepValue('from',
+                    call_on = lambda x:
+                        self.abstract_eval(body, {
+                            k:v for k,v in
+                            list(environ.items())+[(var_nm, x)]
+                        }, depth+1),
+                    default =  
+                        self.abstract_eval(body, {
+                            k:v for k,v in
+                            list(environ.items())+[(var_nm, var_type.build())]
+                        }, depth+1)
+                )
         elif type(tree) == list:
             head, args = tree[0], tree[1:]
-
+    
             if head=='pair':
                 return ( 
                     self.abstract_eval(args[0], environ, depth+1).pair(
@@ -129,11 +193,7 @@ class DependencyAnalyzer:
                 for arg in args:
                     aa = self.abstract_eval(arg, environ, depth+1)
                     self.report('|arg', aa, arg, environ, depth)
-                    partial = (
-                        partial.call_on(aa)
-                        if partial.kind=='from' else
-                        partial.union(aa)
-                    )
+                    partial = partial.call_on(aa)
                 self.report(':ans', partial, tree, environ, depth)
                 return partial
         else:
@@ -143,57 +203,130 @@ if __name__=='__main__':
     verbose = (len(sys.argv)==2 and sys.argv[1] == '-v')
 
     sensitives = {'NOISE', 'MORE_NOISE', 'EVEN_MORE_NOISE'}
-    sigs_by_nm = {}
-    #sigs_by_nm = {
-    #    'mix': (base.pair(base)).frm(base.pair(base)).frm(base.pair(base))
-    #}
-    IA = DependencyAnalyzer(sensitives, sigs_by_nm, verbose=verbose)
+    sigs_by_nm = {
+        'rac3': tBase.frm(tBase).frm(tBase).frm(tBase),
+        'rac2': tBase.frm(tBase).frm(tBase),
+        'rac1': tBase.frm(tBase),
+        'cow': tBase,
+        'mix': tBase.frm(tBase.pair(tBase)),
+        'mmix': (tBase.pair(tBase)).frm(tBase.pair(tBase)).frm(tBase.pair(tBase))
+    }
+    environ = {
+        k: v.build()
+        for k,v in sigs_by_nm.items()
+    }
+    IA = DepAnalyzer(sensitives, environ, verbose=verbose)
         
-    tree_depset_pairs = [
-        (
-            ['pair', 'MORE_NOISE', 'NOISE'],
-            base('MORE_NOISE').pair(base('NOISE'))
-        ),
-        (
-            ['fst', ['pair', 'MORE_NOISE', 'NOISE']],
-            base('MORE_NOISE')
-        ),
-        #(
-        #    ['mix',
-        #        ['pair', 'NOISE', 'NOISE'],
-        #        ['pair', 'MORE_NOISE', 'NOISE']
-        #    ],
-        #    (set(['NOISE', 'MORE_NOISE']), set(['NOISE', 'MORE_NOISE']))
-        #),
-        (
-            ['MORE_NOISE'],
-            base('MORE_NOISE')
-        ),
-        (
-            ['rac', [{('x', 0): ['cow']}, 'MORE_NOISE'], 'NOISE', 'cow'],
-            base('NOISE')
-        ),
-        (
-            [
-                {('x', None): {('y', None): ['cow', 'x', 'y']}},
-                'MORE_NOISE', 'NOISE'
-            ],
-            base('NOISE', 'MORE_NOISE')
-        ),
-        (
-            [
-                {('x', None): {('y', None): ['y', 'x']}},
-                [{('x', None): {('y', None): 'x'}}, 'NOISE', 'MORE_NOISE'],
-                {('x', None): 'rac'}
-            ],
-            base()
-        )
-    ]
-    for tree, dependency_set in tree_depset_pairs:
-        print(CC+'testing on @P {}@D '.format(tree))
-        print(CC+'expected @O {}@D '.format(dependency_set))
-        predicted = IA.abstract_eval(tree, {})
-        pre(predicted==dependency_set, 'failed test!')
-        print(CC+'@G passed!@D \n')
+    tree_depset_pairs = {
+        'basic': [ 
+            (
+                ['MORE_NOISE'],
+                base('MORE_NOISE')
+            ),
+            (
+                [
+                    'rac3',
+                    [{('x', tBase): 'cow'}, 'MORE_NOISE'],
+                    'NOISE',
+                    'cow'
+                ],
+                base('NOISE')
+            ),
+            (
+                [
+                    {('x', tBase): {('y', tBase): ['rac2', 'x', 'y']}},
+                    'MORE_NOISE', 'NOISE'
+                ],
+                base('NOISE', 'MORE_NOISE')
+            ),
+            (
+                [
+                    {('y', tBase.frm(tBase)): ['y', 'NOISE']},
+                    {('x', tBase): 'cow'}
+                ],
+                base()
+            ),
+            (
+                [
+                    {('x', tBase): {('y', tBase.frm(tBase)): ['y', 'x']}},
+                    [{('x', tBase): {('y', tBase): 'x'}}, 'NOISE', 'MORE_NOISE'],
+                    {('x', tBase): 'cow'}
+                ],
+                base()
+            )
+        ],
+        'product types': [
+            (
+                ['pair', 'MORE_NOISE', 'NOISE'],
+                base('MORE_NOISE').pair(base('NOISE'))
+            ),
+            (
+                ['fst', ['pair', 'MORE_NOISE', 'NOISE']],
+                base('MORE_NOISE')
+            ),
+        ],
+        'blackbox primitives': [
+            (
+                ['mix', ['pair', 'NOISE', 'NOISE']],
+                base('NOISE')
+            ),
+            (
+                ['mmix',
+                    ['pair', 'NOISE', 'NOISE'],
+                    ['pair', 'MORE_NOISE', 'NOISE']
+                ],
+                base('NOISE', 'MORE_NOISE').pair(base('NOISE', 'MORE_NOISE'))
+            ),
+        ],
+        'products of functions': [
+            (
+                [
+                    ['fst', ['pair',
+                        {('x', tBase): 'NOISE'},
+                        {('x', tBase): 'MORE_NOISE'},
+                    ]],
+                    ['MORE_NOISE']
+                ],
+                base('NOISE')
+            ),
+            (
+                [
+                        ['snd', ['pair',
+                            {('x', tBase.pair(tBase)): ['fst', 'x']},
+                            {('x', tBase.pair(tBase)): ['snd', 'x']},
+                        ]],
+                        ['pair', 'NOISE', 'MORE_NOISE']
+                ],
+                base('MORE_NOISE')
+            ),
+            (
+                [
+                    [
+                        ['snd', ['pair',
+                            {('x', tBase.pair(tBase)): ['fst', 'x']},
+                            {('x', tBase.pair(tBase)): ['snd', 'x']},
+                        ]],
+                        ['pair',
+                            {('x', tBase): {('y', tBase): ['pair', 'NOISE'     , 'cow']}},
+                            {('x', tBase): {('y', tBase): ['pair', 'MORE_NOISE', 'cow']}},
+                        ]
+                    ],
+                    'cow',
+                    'cow'
+                ],
+                base('MORE_NOISE').pair(base())
+            ),
+        ],
+    }
+
+    print()
+    for test_nm, pairs in tree_depset_pairs.items():
+        print(CC+'@R --- TESTING {} ---@D '.format(test_nm))
+        for tree, dependency_set in pairs:
+            print(CC+'analyze the tree @P {}@D '.format(tree))
+            print(CC+'expect dependencies @O {}@D '.format(dependency_set))
+            predicted = IA.abstract_eval(tree, environ)
+            pre(predicted==dependency_set, 'failed test!')
+            print(CC+'@G passed!@D \n')
     
     print('all tests executed!\n')
