37d1c7c3b6f27367117cb9f4c37e3a0882dc396b
Samuel <samuel@umich.edu>
2020-02-29 19:47:08 -0500

    refined cond, map, fold, filter, repeat pass tests

--- inject.py
+++ inject.py
@@ -20,6 +20,10 @@ from fit_weights import WeightLearner
 from resources import PrimitivesWrapper
 from vis import str_from_grids, render_color
 
+ROLLOUT = 4
+# TODO: convergence check so that rollout unneeded (subtle: need to check
+# convergence across all tree nodes!!
+
 class DepType:
     def __init__(self, kind, **kwargs):
         pre(kind in ['base', 'from', 'pair'], 'unknown kind `{}`'.format(kind)) 
@@ -45,7 +49,6 @@ class DepType:
             return '({} <- {})'.format(repr(self.out), repr(self.arg))
 
 
-
     def build(self, leaves=set()):
         '''
             Return value of this type maximally sensitive to given set of
@@ -68,17 +71,6 @@ class DepType:
 
 
 class DepValue:
-
-    def squash(self):
-        '''
-            Return set of ints on which, in isolation, this depvalue depends
-        '''
-        if self.kind=='base':
-            return self.members
-        elif self.kind == 'pair':
-            return self.fst.squash().union(self.snd.squash())
-        elif self.kind=='from':
-            return self.default.squash() 
 
     def __init__(self, kind, **kwargs):
         pre(kind in ['base', 'from', 'pair'], 'unknown kind `{}`'.format(kind)) 
@@ -109,7 +101,74 @@ class DepValue:
             return 'F{}'.format(repr(self.default))
 
     def __eq__(self, rhs):
-        return repr(self)==repr(rhs)
+        selfr, rhsr = repr(self), repr(rhs)
+        pre('F' not in selfr+rhsr, 'cannot compare function types!')
+        return selfr==rhsr
+
+    def squash(self):
+        '''
+            Return set of ints on which, in isolation, this depvalue depends
+        '''
+        if self.kind=='base':
+            return self.members
+        elif self.kind == 'pair':
+            return self.fst.squash().union(self.snd.squash())
+        elif self.kind=='from':
+            return self.default.squash() 
+
+    def union_leaf(self, intset):
+        if self.kind=='base':
+            return DepValue('base',
+                members = self.members.union(intset)
+            )
+        elif self.kind == 'pair':
+            return DepValue('pair',
+                fst = self.fst.union_leaf(intset),
+                snd = self.snd.union_leaf(intset)
+            )
+        elif self.kind=='from':
+            return DepValue('from',
+                call_on = lambda x: self.call_on(x).union_leaf(intset),
+                default = self.default.union_leaf(intset)
+            )
+
+    def union(self, rhs):
+        if self.kind=='base': 
+            return DepValue('base', members = self.members.union(rhs.members))
+        elif self.kind=='pair':
+            return DepValue('pair',
+                fst = self.fst.union(rhs.fst),
+                snd = self.snd.union(rhs.snd)
+            )
+        elif self.kind=='from':
+            return DepValue('from',
+                call_on = lambda x: self.call_on(x).union(rhs.call_on(x)),
+                default = self.default.union(rhs.default)
+            )
+
+    # TODO: refactor:
+    @staticmethod
+    def nested_union(init, seq):
+        '''
+        '''
+        if init.kind=='base': 
+            members = init.members
+            for elt in seq():
+                new_members = members.union(elt.members)
+                members = new_members
+            return DepValue('base',
+                members = members
+            )
+        elif init.kind=='pair':
+            return DepValue('pair',
+                fst = DepValue.nested_union(init.fst, lambda:(elt.fst for elt in seq())),
+                snd = DepValue.nested_union(init.snd, lambda:(elt.snd for elt in seq())),
+            )
+        elif init.kind=='from':
+            return DepValue('from',
+                call_on = lambda x: DepValue.nested_union(init.call_on(x), lambda:(elt.call_on(x) for elt in seq())), 
+                default = DepValue.nested_union(init.default, lambda:(elt.default for elt in seq())), 
+            )
 
 tBase = DepType('base')
 
@@ -161,21 +220,55 @@ class DepAnalyzer:
                 )
         elif type(tree) == list:
             head, args = tree[0], tree[1:]
+
+            eval_child = lambda t: self.abstract_eval(t, environ, depth+1)
     
             if head=='pair':
-                return ( 
-                    self.abstract_eval(args[0], environ, depth+1).pair(
-                    self.abstract_eval(args[1], environ, depth+1)
-                ))
+                fst, snd = map(eval_child, args)
+                return fst.pair(snd)
             elif head=='fst':
-                return self.abstract_eval(args[0], environ, depth+1).fst
-            elif head=='snd':                                       
-                return self.abstract_eval(args[0], environ, depth+1).snd
+                pair, = map(eval_child, args)
+                return pair.fst
+            elif head=='snd':
+                pair, = map(eval_child, args)
+                return pair.snd
+            elif head=='cond':
+                cond, if_tru, if_fls = map(eval_child, args)
+                return if_tru.union(if_fls).union_leaf(cond.squash())
+            elif head=='map':
+                elts, func = map(eval_child, args)
+                return func.call_on(elts)
+            elif head=='filter':
+                pred, elts = map(eval_child, args)
+                return elts.union_leaf(pred.squash())
+            elif head=='repeat':
+                reps, init, func = map(eval_child, args)
+                def rep_appl_generator():
+                    x = init
+                    for _ in range(ROLLOUT):
+                        x = func.call_on(x)
+                        yield x
+                return DepValue.nested_union(
+                    init = init,
+                    seq = rep_appl_generator
+                ).union_leaf(reps.squash())
+            elif head=='fold':
+                elts, init, func = map(eval_child, args)
+                action = func.call_on(elts)
+                def rep_appl_generator():
+                    x = init
+                    for _ in range(ROLLOUT):
+                        x = action.call_on(x)
+                        yield x
+                return DepValue.nested_union(
+                    init = init,
+                    seq = rep_appl_generator
+                ).union_leaf(elts.squash())
             else:
-                partial = self.abstract_eval(head, environ, depth+1)
+                partial = eval_child(head)
                 self.report('|cal', partial, head, environ, depth)
                 for arg in args:
-                    aa = self.abstract_eval(arg, environ, depth+1)
+                    aa = eval_child(arg)
                     self.report('|arg', aa, arg, environ, depth)
                     partial = partial.call_on(aa)
                 self.report(':ans', partial, tree, environ, depth)
@@ -186,7 +279,7 @@ class DepAnalyzer:
 if __name__=='__main__':
     verbose = (len(sys.argv)==2 and sys.argv[1] == '-v')
 
-    sensitives = {'NOISE', 'MORE_NOISE', 'EVEN_MORE_NOISE'}
+    sensitives = {'NOISE', 'DIN', 'SOUND'}
     sigs_by_nm = {
         'rac3': tBase.frm(tBase).frm(tBase).frm(tBase),
         'rac2': tBase.frm(tBase).frm(tBase),
@@ -203,10 +296,14 @@ if __name__=='__main__':
     IA = DepAnalyzer(sensitives, environ, verbose=verbose)
         
     tree_depset_pairs = {
-        'basic': [ 
+        'plain lambda calculus': [ 
             (
-                ['MORE_NOISE'],
-                base('MORE_NOISE')
+                ['cow'],
+                base()
+            ),
+            (
+                ['NOISE'],
+                base('NOISE')
             ),
             (
                 [
@@ -218,20 +315,31 @@ if __name__=='__main__':
             (
                 [
                     {('x', tBase): {('y', tBase.frm(tBase)): ['y', 'x']}},
-                    [{('x', tBase): {('y', tBase): 'x'}}, 'NOISE', 'MORE_NOISE'],
+                    [{('x', tBase): {('y', tBase): 'x'}}, 'NOISE', 'DIN'],
                     {('x', tBase): 'cow'}
                 ],
                 base()
-            )
+            ),
+            (
+                [
+                    {('x', tBase): {('y', tBase):
+                        {('z', tBase.frm(tBase).frm(tBase)): ['z', 'x', 'y']}
+                    }},
+                    'DIN',
+                    'NOISE',
+                    {('x', tBase): {('y', tBase): 'x'}},
+                ],
+                base('DIN')
+            ),
         ],
         'product types': [
             (
-                ['pair', 'MORE_NOISE', 'NOISE'],
-                base('MORE_NOISE').pair(base('NOISE'))
+                ['pair', 'DIN', 'NOISE'],
+                base('DIN').pair(base('NOISE'))
             ),
             (
-                ['fst', ['pair', 'MORE_NOISE', 'NOISE']],
-                base('MORE_NOISE')
+                ['fst', ['pair', 'DIN', 'NOISE']],
+                base('DIN')
             ),
             (
                 [
@@ -244,7 +352,7 @@ if __name__=='__main__':
                         }
                     },
                     {('x', tBase): 'NOISE'},
-                    ['pair', 'MORE_NOISE', 'cow']
+                    ['pair', 'DIN', 'cow']
                 ],
                 base('NOISE').pair(base('NOISE'))
             ),
@@ -252,9 +360,9 @@ if __name__=='__main__':
                 [
                     ['fst', ['pair',
                         {('x', tBase): 'NOISE'},
-                        {('x', tBase): 'MORE_NOISE'},
+                        {('x', tBase): 'DIN'},
                     ]],
-                    ['MORE_NOISE']
+                    ['DIN']
                 ],
                 base('NOISE')
             ),
@@ -267,20 +375,20 @@ if __name__=='__main__':
                         ]],
                         ['pair',
                             {('x', tBase): {('y', tBase): ['pair', 'NOISE'     , 'cow']}},
-                            {('x', tBase): {('y', tBase): ['pair', 'MORE_NOISE', 'cow']}},
+                            {('x', tBase): {('y', tBase): ['pair', 'DIN', 'cow']}},
                         ]
                     ],
                     'cow',
                     'cow'
                 ],
-                base('MORE_NOISE').pair(base())
+                base('DIN').pair(base())
             ),
         ],
         'blackbox primitives': [
             (
                 [
                     'rac3',
-                    [{('x', tBase): 'cow'}, 'MORE_NOISE'],
+                    [{('x', tBase): 'cow'}, 'DIN'],
                     'NOISE',
                     'cow'
                 ],
@@ -288,10 +396,14 @@ if __name__=='__main__':
             ),
             (
                 [
-                    {('x', tBase): {('y', tBase): ['rac2', 'x', 'y']}},
-                    'MORE_NOISE', 'NOISE'
+                    {('x', tBase): {('y', tBase):
+                        {('z', tBase.frm(tBase).frm(tBase)): ['z', 'x', 'y']}
+                    }},
+                    'DIN',
+                    'NOISE',
+                    'rac2',
                 ],
-                base('NOISE', 'MORE_NOISE')
+                base('NOISE', 'DIN')
             ),
             (
                 ['mix', ['pair', 'NOISE', 'NOISE']],
@@ -300,16 +412,72 @@ if __name__=='__main__':
             (
                 ['mmix',
                     ['pair', 'NOISE', 'NOISE'],
-                    ['pair', 'MORE_NOISE', 'NOISE']
+                    ['pair', 'DIN', 'NOISE']
                 ],
-                base('NOISE', 'MORE_NOISE').pair(base('NOISE', 'MORE_NOISE'))
+                base('NOISE', 'DIN').pair(base('NOISE', 'DIN'))
             ),
             (
                 ['mymap',
-                    ['pair', 'NOISE', 'MORE_NOISE'],
+                    ['pair', 'NOISE', 'DIN'],
                     'rac1',
                 ],
-                base('NOISE', 'MORE_NOISE').pair(base('NOISE', 'MORE_NOISE'))
+                base('NOISE', 'DIN').pair(base('NOISE', 'DIN'))
+            ),
+        ],
+        'refined operators': [
+            (
+                ['cond',
+                    ['rac2', 'cow', 'DIN'],
+                    ['pair', 'SOUND', 'cow'],
+                    ['pair', 'NOISE', 'cow'],
+                ],
+                base('DIN', 'SOUND', 'NOISE').pair(base('DIN'))
+            ),
+            (
+                [
+                    'map',
+                    'SOUND',
+                    {('x', tBase): ['pair', ['pair', 'x', 'cow'], 'NOISE']},
+                ],
+                (base('SOUND').pair(base())).pair(base('NOISE'))
+            ),
+            (
+                ['repeat',
+                    'SOUND',
+                    ['pair', 'NOISE', ['pair', 'DIN', 'cow']],
+                    {('x', tBase.pair(tBase.pair(tBase))):
+                        ['pair',
+                            ['fst', ['snd', 'x']],
+                            ['pair', ['fst', ['snd', 'x']], ['fst', 'x']],
+                        ]
+                    }
+                ],
+                base('NOISE', 'DIN', 'SOUND').pair(
+                    base('DIN', 'SOUND').pair(
+                    base('NOISE', 'DIN', 'SOUND')
+                ))
+            ),
+            (
+                [
+                    ['fold',
+                        'NOISE',
+                        {('x',tBase): ['pair', 'x', 'DIN']},
+                        {('elt',tBase): {('f', (tBase.pair(tBase)).frm(tBase)):
+                            {('x',tBase):
+                                ['pair', ['fst', ['f', 'x']], 'elt']
+                            }
+                        }}
+                    ],
+                    'SOUND'
+                ],
+                base('NOISE', 'SOUND').pair(base('DIN', 'NOISE'))
+            ),
+            (
+                ['filter',
+                    {('x',tBase): 'SOUND'},
+                    ['pair', 'DIN', 'cow']
+                ],
+                base('DIN', 'SOUND').pair(base('SOUND'))
             ),
         ],
     }
@@ -318,10 +486,11 @@ if __name__=='__main__':
     for test_nm, pairs in tree_depset_pairs.items():
         print(CC+'@R --- TESTING {} ---@D '.format(test_nm))
         for tree, dependency_set in pairs:
-            print(CC+'analyze the tree @P {}@D '.format(tree))
-            print(CC+'expect dependencies @O {}@D '.format(dependency_set))
+            print(CC+'analyze tree @P {}@D : expect dependencies @O {}@D '.format(
+                tree, dependency_set
+            ))
             predicted = IA.abstract_eval(tree, environ)
             pre(predicted==dependency_set, 'failed test!')
-            print(CC+'@G passed!@D \n')
+            print(CC+'@G passed!@D ')
     
     print('all tests executed!\n')
